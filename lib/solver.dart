7426abc07681c40053bf3e556a9c074bba8acf0c
Mark Goodwin <m0good04@gmail.com>
2024-01-17 09:47:50 -0500

    Added a static Solver class to solve puzzles.

--- lib/solver.dart
+++ lib/solver.dart
@@ -1,3 +1,25 @@
-class Solver {
+import 'package:sudoku/shared.dart';
 
+class Solver {
+  static solvePuzzle(List<List<int>> puzzle) {
+    if (Shared.isSolved(puzzle)) {
+      return true;
+    }
+
+    Location emptyCell = Shared.findEmptyCell(puzzle);
+
+    for (int number = 1; number <= 9; number++) {
+      if (Shared.isValidPlacement(puzzle, emptyCell.row, emptyCell.column, number)) {
+        puzzle[emptyCell.row][emptyCell.column] = number;
+
+        if (solvePuzzle(puzzle)) {
+          return true;
+        }
+
+        puzzle[emptyCell.row][emptyCell.column] = 0;
+      }
+    }
+
+    return false;
+  }
 }
---
+++ test/solver_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sudoku/solver.dart';
+
+void main() {
+  test('test Solver.solvePuzzle() when puzzle can be solved', () {
+    List<List<int>> puzzle = [
+      [0, 0, 4, 3, 0, 0, 2, 0, 9],
+      [0, 0, 5, 0, 0, 9, 0, 0, 1],
+      [0, 7, 0, 0, 6, 0, 0, 4, 3],
+      [0, 0, 6, 0, 0, 2, 0, 8, 7],
+      [1, 9, 0, 0, 0, 7, 4, 0, 0],
+      [0, 5, 0, 0, 8, 3, 0, 0, 0],
+      [6, 0, 0, 0, 0, 0, 1, 0, 5],
+      [0, 0, 3, 5, 0, 8, 6, 9, 0],
+      [0, 4, 2, 9, 1, 0, 3, 0, 0],
+    ];
+
+    expect(Solver.solvePuzzle(puzzle), true);
+
+    List<List<int>> expected = [
+      [8, 6, 4, 3, 7, 1, 2, 5, 9],
+      [3, 2, 5, 8, 4, 9, 7, 6, 1],
+      [9, 7, 1, 2, 6, 5, 8, 4, 3],
+      [4, 3, 6, 1, 9, 2, 5, 8, 7],
+      [1, 9, 8, 6, 5, 7, 4, 3, 2],
+      [2, 5, 7, 4, 8, 3, 9, 1, 6],
+      [6, 8, 9, 7, 3, 4, 1, 2, 5],
+      [7, 1, 3, 5, 2, 8, 6, 9, 4],
+      [5, 4, 2, 9, 1, 6, 3, 7, 8],
+    ];
+
+    for (int r = 0; r < 9; r++) {
+      for (int c = 0; c < 9; c++) {
+        expect(puzzle[r][c], expected[r][c]);
+      }
+    }
+  });
+
+  test('test Solver.solvePuzzle() when puzzle cannot be solved', () {
+    List<List<int>> puzzle = [
+      [2, 0, 0, 9, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 6, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 6, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [5, 0, 2, 6, 0, 0, 4, 0, 7],
+      [0, 0, 0, 0, 0, 4, 1, 0, 0],
+      [0, 0, 0, 0, 9, 8, 0, 2, 3],
+      [0, 0, 0, 0, 0, 3, 0, 8, 0],
+      [0, 0, 5, 0, 1, 0, 0, 0, 0],
+    ];
+
+    expect(Solver.solvePuzzle(puzzle), false);
+  });
+}
